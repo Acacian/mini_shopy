6d40d12b2f2ff8f5fbeaf3f386535dad1294aa48
DOHA <koo9811@gmail.com>
2024-06-19 14:30:09 +0900

    frontend complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import { UserContextProvider } from "../context/UserContext";
-import CartStatus from "../components/CartStatus";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Header from './components/Header'; // 수정된 경로
+import { UserContextProvider } from './context/UserContext'; // 수정된 경로
+import CartStatus from './components/CartStatus'; // 수정된 경로
 
+// QueryClient 인스턴스를 생성합니다.
 const queryClient = new QueryClient();
-export default function App() {
+
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <UserContextProvider>
         <Header />
-        <CartStatus /> 
+        <CartStatus />
         <Outlet />
       </UserContextProvider>
     </QueryClientProvider>
   );
 }
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import ErrorPage from '../pages/ErrorPage';
-import Home from '../pages/Home';
-import Products from '../pages/Products';
-import ProductDetail from '../pages/ProductDetail';
-import MyCart from '../pages/MyCart';
-import AddProduct from '../pages/AddProduct';
-import ProtectedRoute from '../components/ProtectedRoute';
+import ErrorPage from './pages/ErrorPage'; // 경로 수정
+import Home from './pages/Home'; // 경로 수정
+import Products from './pages/Products'; // 경로 수정
+import ProductDetail from './pages/ProductDetail'; // 경로 수정
+import MyCart from './pages/MyCart'; // 경로 수정
+import AddProduct from './pages/AddProduct'; // 경로 수정
+import ProtectedRoute from './components/ProtectedRoute'; // 경로 수정
 
 const router = createBrowserRouter([
   {
